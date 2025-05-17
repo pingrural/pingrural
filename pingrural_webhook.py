075b570c9ee196b376c4e05d635d040d85d261e6
pingrural <rodrigrech@hotmail.com>
2025-05-17 18:01:41 -0400

    Cria arquivo principal do webhook

--- pingrural_webhook.py
+++ pingrural_webhook.py
@@ -1,10 +1,26 @@
-# Arquivo principal do webhook
 from flask import Flask, request
+
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+VERIFY_TOKEN = "pingrural123"
+
+@app.route('/', methods=['GET', 'POST'])
 def webhook():
-    return "Ping Rural webhook funcionando!"
+    if request.method == 'GET':
+        mode = request.args.get("hub.mode")
+        token = request.args.get("hub.verify_token")
+        challenge = request.args.get("hub.challenge")
+
+        if mode == "subscribe" and token == VERIFY_TOKEN:
+            print("✅ Webhook verificado com sucesso!")
+            return challenge, 200
+        else:
+            return "Token inválido", 403
+
+    elif request.method == 'POST':
+        data = request.get_json()
+        print("📨 Mensagem recebida:", data)
+        return "Mensagem recebida", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
